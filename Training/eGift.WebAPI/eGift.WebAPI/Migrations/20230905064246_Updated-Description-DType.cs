66dab5821b6b4d566b6803de47e689b868b323fc
Nikhat Shazi <shazi.hbt@gmail.com>
2025-09-02 16:38:09 +0530

    eGift Web API backend  application.

---
+++ Training/eGift.WebAPI/eGift.WebAPI/Migrations/20230905064246_Updated-Description-DType.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eGift.WebAPI.Migrations
+{
+    public partial class UpdatedDescriptionDType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "dbo",
+                table: "Category",
+                type: "varchar(500)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "dbo",
+                table: "Category",
+                type: "varchar(500)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldNullable: true);
+        }
+    }
+}
